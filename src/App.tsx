678aac6d118bd77c236c935ca898924e64a8a6d2
Quddus Salam <quddussalam555@gmail.com>
2023-09-02 12:47:51 +0700

    Added notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+/* Copyright 2023 XnonXte. */
+
+/*
+Notes:
+  - This project uses Bootstrap (via CDN) and React using vite.
+  - This project has been deployed to GitHub Pages on https://xnonxte.github.io/store-app-deploy/
+  - This project is created in under 2 hours?
+  - Possible extension to this project including, but not limited to:
+    1. Add a rating system.
+    2. Create a more robust card system.
+    3. Create a customized bootstrap SCSS.
+    4. Add a landing page?
+    5. Add a user page (including user auth of course).
+*/
+
 import { useEffect, useState } from "react";
 import StoreList from "./components/StoreList";
 import Navbar from "./components/Navbar";
